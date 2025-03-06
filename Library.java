30b947e7825972cfaa72d54d4e7af83412b0368a
Adir <105810536+AdirAdam@users.noreply.github.com>
2025-03-06 14:02:17 -0500

    finished the library file 3/6/2025

---
+++ Library.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Library {
+    ArrayList <String> collection = new ArrayList<String>();
+
+    public Library(ArrayList <String> collection) {
+        this.collection = collection;
+    }
+
+    public void addItem(String addItem) {
+        collection.add(addItem);
+    }
+
+    public void removeItem(String removeItem) {
+        for(String x: collection) {
+            if (x.equals(removeItem)) {
+                collection.remove(x);
+            }
+        }
+    }
+
+    public String getNextItem() {
+        return collection.get(0);
+    }
+
+    public void skipNextItem() {
+        collection.remove(0);
+    }
+
+    public void sortAlphabet() {
+        collection.sort(null);
+    }
+
+    public int find(String searchItem) {
+        return collection.indexOf(searchItem);
+    }
+
+    public void shuffle() {
+        Collections.shuffle(collection);
+    }
+
+
+}
