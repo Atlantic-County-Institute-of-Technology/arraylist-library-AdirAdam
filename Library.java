1246f0229c050d07eaf2761566489c93d96e2fb4
Adir <105810536+AdirAdam@users.noreply.github.com>
2025-03-07 14:12:15 -0500

    finished both files & commented

--- Library.java
+++ Library.java
@@ -1,19 +1,20 @@
+// Adir Adam 3.7.2025 Library.java
 import java.util.ArrayList;
 import java.util.Collections;
 
 
-public class Library {
-    ArrayList <String> collection = new ArrayList<String>();
+public class Library {      // Library class used as a collection of items object
+    ArrayList <String> collection = new ArrayList<String>();            // Local variable to manage ArrayList
 
-    public Library() {
+    public Library() {                          // Constructor for the collection variable
         this.collection = collection;
     }
 
-    public void addItem(String addItem) {
+    public void addItem(String addItem) {       // Adds an item to the collection
         collection.add(addItem);
     }
 
-    public void removeItem(String removeItem) {
+    public void removeItem(String removeItem) { // Removes an item through a for each loop
         for(String x: collection) {
             if (x.equals(removeItem)) {
                 collection.remove(x);
@@ -21,24 +22,24 @@ public class Library {
         }
     }
 
-    public String getNextItem() {
+    public String getNextItem() {   // Returns the first item
         return collection.get(0);
     }
 
-    public void skipNextItem() {
+    public void skipNextItem() {    // Removes the first item
         collection.remove(0);
     }
 
-    public void sortAlphabet() {
+    public void sortAlphabet() {    // Sorts the collection alphabetically through .sort
         collection.sort(null);
     }
 
-    public int find(String searchItem) {
+    public int find(String searchItem) {    // Searches for the index of a specified item
         return collection.indexOf(searchItem);
     }
 
     public void shuffle() {
-        Collections.shuffle(collection);    // ues
+        Collections.shuffle(collection);    // Shuffles the Arraylist
     }
 
 
--- Main.java
+++ Main.java
@@ -1,14 +1,15 @@
+// Adir Adam 3.7.2025 Main.java
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Library library = new Library();
-        Scanner userInput = new Scanner(System.in);
-        String currentItem = "";
+        Library library = new Library();                // Gets the stuff from the Library.java file
+        Scanner userInput = new Scanner(System.in);     // Gets the Scanner
+        String currentItem = "";        // Current item that is "playing"
 
-        while (true) {
+        while (true) {      // Program loops unless the user wants to stop
             try {
-                System.out.println("\nLibrary Menu:");
+                System.out.println("\nLibrary Menu:");      // Declares the "Main Menu"
                 System.out.println("1. Play");
                 System.out.println("2. Skip");
                 System.out.println("3. Add");
@@ -20,11 +21,11 @@ public class Main {
                 System.out.println("9. Exit");
                 System.out.print("Choose an option: ");
 
-                int choice = userInput.nextInt();
-                userInput.nextLine(); // Consume newline
+                int choice = userInput.nextInt();   // Asks for user inputs
+                userInput.nextLine();
 
-                switch (choice) {
-                    case 1:
+                switch (choice) {          // Switch case so I don't have to do "if 1, if 2, etc etc"
+                    case 1:     // This checks the current item
                         currentItem = library.getNextItem();
                         if (!currentItem.equals("Library is empty")) {
                             library.skipNextItem();
@@ -33,31 +34,31 @@ public class Main {
                             System.out.println("Library is empty.");
                         }
                         break;
-                    case 2:
+                    case 2:     // This skips the current item
                         library.skipNextItem();
                         System.out.println("Skipped to the next item.");
                         break;
-                    case 3:
+                    case 3:     // Allows the user to add an item to the queue
                         System.out.print("Enter item to add: ");
                         String addItem = userInput.nextLine();
                         library.addItem(addItem);
                         System.out.println(addItem + " added to library.");
                         break;
-                    case 4:
+                    case 4:     // Allows the user to remove an item to the queue
                         System.out.print("Enter item to remove: ");
                         String removeItem = userInput.nextLine();
                         library.removeItem(removeItem);
                         System.out.println(removeItem + " removed from library (if it existed).");
                         break;
-                    case 5:
+                    case 5:     // Sorts the list alphabetically & numerically
                         library.sortAlphabet();
                         System.out.println("Library sorted alphabetically.");
                         break;
-                    case 6:
+                    case 6:     // Shuffles the list randomly
                         library.shuffle();
                         System.out.println("Library shuffled.");
                         break;
-                    case 7:
+                    case 7:     // Finds the position of the item the user wants
                         System.out.print("Enter item to find: ");
                         String searchItem = userInput.nextLine();
                         int position = library.find(searchItem);
@@ -67,7 +68,7 @@ public class Main {
                             System.out.println(searchItem + " not found in library.");
                         }
                         break;
-                    case 8:
+                    case 8:     // Allows the user to see whats in the queue
                         System.out.println("Library Queue:");
                         if (library.collection.isEmpty()) {
                             System.out.println("Library is empty.");
@@ -77,14 +78,14 @@ public class Main {
                             }
                         }
                         break;
-                    case 9:
+                    case 9:     // Exits the program
                         System.out.println("Goodbye.");
                         userInput.close();
                         return;
-                    default:
+                    default:    // if the user does an invalid integer
                         System.out.println("Please select a valid input!");
                 }
-            } catch (Exception e) {
+            } catch (Exception e) {     // if the user does an invalid character
                 System.out.println("Invalid input. Please enter a number.");
                 userInput.nextLine(); // Clear invalid input
             }
